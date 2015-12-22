f76c3d1ff25c3ec1d25f0c713d25dc8f16803a3b
Manassorn <manassorn@gmail.com>
2015-12-22 23:57:02 +0700

    songdb.js

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ var jukebox = {
   },
   load: function(callback) {
     var r = new XMLHttpRequest(); 
-    r.open("GET", "songdb.txt", true);
+    r.open("GET", "songdb.js", true);
     r.onreadystatechange = function () {
       if (r.readyState != 4 || r.status != 200) {
         return; 
